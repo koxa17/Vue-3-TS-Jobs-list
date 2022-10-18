a50086ead597a8150d47fb8f64009f70a36d60b4
koxa17 <n.matusevich@clicktoid.ru>
2022-10-18 07:35:48 +0500

    rollback deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'Deploy'
-git push -f git@github.com:koxa17/Vue-3-TS-Jobs-list.git main:gh-pages
+git push -f git@github.com:koxa17/Vue-3-TS-Jobs-list.git master:gh-pages
 
 cd -
